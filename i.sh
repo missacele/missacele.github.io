8bd1e2bbd4dffd4590e82ae913769a3ef1eaaaa3
missacele <210659948+missacele@users.noreply.github.com>
2025-11-21 13:54:10 +0800

    Update i.sh

--- i.sh
+++ i.sh
@@ -355,13 +355,10 @@ else
   rm -f /tmp/code.deb
 fi
 
-if ! dpkg -s code >/dev/null 2>&1 || ! code --list-extensions | grep -q "biomejs.biome"; then
+if ! dpkg -s code >/dev/null 2>&1 || ! code --list-extensions | grep -q "PKief.material-icon-theme"; then
   log_info "Installing VS Code extensions"
-  code --install-extension biomejs.biome
   code --install-extension PKief.material-icon-theme
   code --install-extension qwtel.sqlite-viewer
-  code --install-extension bradlc.vscode-tailwindcss
-  code --install-extension Vue.volar
 else
   log_info "VS Code extensions already installed"
 fi
@@ -387,7 +384,6 @@ cat > "$HOME/.config/Code/User/settings.json" <<EOF
   "terminal.integrated.fontFamily": "'FiraCode Nerd Font', monospace",
   "window.commandCenter": false,
   "window.newWindowDimensions": "maximized",
-  "workbench.activityBar.location": "top",
   "workbench.editor.empty.hint": "hidden",
   "workbench.iconTheme": "material-icon-theme",
   "workbench.startupEditor": "none"
@@ -410,7 +406,6 @@ if [ ! -d "$HOME/.nvm" ]; then
   \. "$HOME/.nvm/nvm.sh"
   nvm install 24
   npm config set fund false
-  npm install -g @biomejs/biome
 else
   log_info "NVM already installed"
 fi
